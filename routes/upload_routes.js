0c49f167b0b6a68ffb368fb26d406e8460543d71
Augusta Phan <pxn.na6923@gmail.com>
2025-08-14 00:53:58 +0700

    change file name

--- routes/upload_routes.js
+++ routes/upload_routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../middlewares/upload");
-router.post("/", upload.single("image"), (req, res) => {
+router.post("/", upload.single("images"), (req, res) => {
     if (!req.file) {
         return res.status(400).json({ message: "No file uploaded" });
     }
     const fileUrl = `/public/message/${req.file.filename}`;
-    res.status(200).json({ url: fileUrl });
+    res.status(200).json({ urls: [fileUrl] });
 });
 module.exports = router;
